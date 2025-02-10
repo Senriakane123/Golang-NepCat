0e39e0877d2e58c5f5dcdab090c1acd6671e58b3
Senriakane123 <65888888A@gmail.com>
2025-02-10 18:07:52 +0800

    sql文件change

--- Database/DatabasSqlFile/gamedata.sql
+++ Database/DatabasSqlFile/gamedata.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : Mysql
+ Source Server         : local
  Source Server Type    : MySQL
  Source Server Version : 80041
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 80041
  File Encoding         : 65001
 
- Date: 10/02/2025 11:58:56
+ Date: 10/02/2025 18:07:05
 */
 
 SET NAMES utf8mb4;
@@ -43,32 +43,19 @@ CREATE TABLE `personalpetinfo`  (
   `PetId` int NOT NULL,
   `PetLevel` int NOT NULL,
   `Exp` int NOT NULL,
+  `QQNum` int NOT NULL,
+  `Skill` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`ID`) USING BTREE,
   INDEX `UserID`(`UserID` ASC) USING BTREE,
   INDEX `PetId`(`PetId` ASC) USING BTREE,
+  INDEX `personalpetinfo_ibfk_3`(`QQNum` ASC) USING BTREE,
   CONSTRAINT `personalpetinfo_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `userinfo` (`ID`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `personalpetinfo_ibfk_2` FOREIGN KEY (`PetId`) REFERENCES `pet` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  CONSTRAINT `personalpetinfo_ibfk_2` FOREIGN KEY (`PetId`) REFERENCES `pet` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `personalpetinfo_ibfk_3` FOREIGN KEY (`QQNum`) REFERENCES `userinfo` (`QQNum`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of personalpetinfo
--- ----------------------------
-
--- ----------------------------
--- Table structure for pesonalskilllist
--- ----------------------------
-DROP TABLE IF EXISTS `pesonalskilllist`;
-CREATE TABLE `pesonalskilllist`  (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `SkillID` int NOT NULL,
-  `PersonalPetID` int NOT NULL,
-  PRIMARY KEY (`ID`) USING BTREE,
-  INDEX `PersonalPetID`(`PersonalPetID` ASC) USING BTREE,
-  CONSTRAINT `pesonalskilllist_ibfk_1` FOREIGN KEY (`PersonalPetID`) REFERENCES `personalpetinfo` (`ID`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of pesonalskilllist
 -- ----------------------------
 
 -- ----------------------------
@@ -94,9 +81,9 @@ CREATE TABLE `pet`  (
 -- ----------------------------
 -- Records of pet
 -- ----------------------------
-INSERT INTO `pet` VALUES (1, '炎龙', '龙类', '火焰喷射', 1.2, 1.5, 1, 1.3, 100, 50, 40, 80);
-INSERT INTO `pet` VALUES (2, '冰狼', '兽类', '寒冰爪击', 1.1, 1.4, 1.2, 1.1, 90, 55, 45, 85);
-INSERT INTO `pet` VALUES (3, '雷鸟', '鸟类', '雷电冲击', 1.3, 1.2, 1.1, 1.4, 110, 45, 50, 90);
+INSERT INTO `pet` VALUES (1, '炎龙', '火', '火焰喷射', 1.2, 1.5, 1, 1.3, 100, 50, 40, 80);
+INSERT INTO `pet` VALUES (2, '冰狼', '冰', '寒冰爪击', 1.1, 1.4, 1.2, 1.1, 90, 55, 45, 85);
+INSERT INTO `pet` VALUES (3, '雷鸟', '雷', '雷电冲击', 1.3, 1.2, 1.1, 1.4, 110, 45, 50, 90);
 
 -- ----------------------------
 -- Table structure for userinfo
@@ -107,8 +94,9 @@ CREATE TABLE `userinfo`  (
   `QQNum` int NOT NULL,
   `Name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Item` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`ID`) USING BTREE,
+  INDEX `QQNum`(`QQNum` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of userinfo
